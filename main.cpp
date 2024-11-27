f1d97de89aad111912567ad2b163133356d713da
Lawrence <laa236@github.edu>
2024-11-27 00:15:57 -0800

    added ability to choose a specific line of input.txt

--- main.cpp
+++ main.cpp
@@ -2,35 +2,70 @@
 #include <chrono>
 #include <fstream>
 #include "smith-waterman.hpp"
+#include <sstream>
 
-void runTests()
+void runTests(int lineNumber)
 {
     std::ifstream inputFile("input.txt");
     std::ofstream outputFile("output.txt");
 
-    std::string seq1, seq2;
-    if (inputFile >> seq1 >> seq2)
-    {
-        // Convert strings to char arrays
-        const char *seq1Ptr = seq1.c_str();
-        const char *seq2Ptr = seq2.c_str();
-        size_t size1 = seq1.length();
-        size_t size2 = seq2.length();
+    if (!inputFile.is_open()) {
+        std::cerr << "Error opening input file!" << std::endl;
+        return;
+    }
 
-        // Call smithWaterman with char pointers and their sizes
-        std::pair<std::string, std::string> out = smithWaterman(seq1Ptr, size1, seq2Ptr, size2);
-        outputFile << out.first << " " << out.second << std::endl;
+    std::string seq1, seq2;
+    std::string line;
+    int currentLine = 1;
+
+    // Read the file line by line
+    while (std::getline(inputFile, line)) {
+        if (currentLine == lineNumber) {
+            std::istringstream lineStream(line);
+            if (lineStream >> seq1 >> seq2) {
+                // Convert strings to char arrays
+                const char *seq1Ptr = seq1.c_str();
+                const char *seq2Ptr = seq2.c_str();
+                size_t size1 = seq1.length();
+                size_t size2 = seq2.length();
+
+                // Call smithWaterman with char pointers and their sizes
+                std::pair<std::string, std::string> out = smithWaterman(seq1Ptr, size1, seq2Ptr, size2);
+                outputFile << out.first << " " << out.second << std::endl;
+            } else {
+                std::cerr << "Error: Line " << lineNumber << " is not properly formatted!" << std::endl;
+            }
+            break;
+        }
+        currentLine++;
+    }
+
+    if (currentLine < lineNumber) {
+        std::cerr << "Error: The input file has fewer lines than the specified line number!" << std::endl;
     }
 
     inputFile.close();
     outputFile.close();
 }
 
-int main()
+int main(int argc, char* argv[])
 {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <lineNumber>" << std::endl;
+        return 1;
+    }
+
+    int lineNumber;
+    try {
+        lineNumber = std::stoi(argv[1]);
+    } catch (const std::invalid_argument& e) {
+        std::cerr << "Error: Invalid line number!" << std::endl;
+        return 1;
+    }
+
     auto start = std::chrono::high_resolution_clock::now();
 
-    runTests();
+    runTests(lineNumber);
 
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end - start;
